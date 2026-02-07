6c07f9f2c374cf3a7a45de8097c3bb6245ff5784
kameoka0221-prog <kameoka0221@gmail.com>
2026-02-07 22:43:27 +0900

    Update Stripe price IDs for fruit items

--- src/main.jsx
+++ src/main.jsx
@@ -71,7 +71,7 @@ const PRODUCTS = [
     acid: 0.8, 
     size: 'L', 
     img: 'https://images.unsplash.com/photo-1611080626919-7cf5a9dcab5b?auto=format&fit=crop&q=80&w=800',
-    stripePriceId: 'price_YOUR_MIKAN_ID' // Stripeダッシュボードで取得したIDに書き換えてください
+    stripePriceId: 'price_1SyBZI1GyBXkmkVBT9lcZcUx' // Stripeダッシュボードで取得したIDに書き換えてください
   },
   { 
     id: 2, 
@@ -81,7 +81,7 @@ const PRODUCTS = [
     acid: 1.5, 
     size: 'M', 
     img: 'https://images.unsplash.com/photo-1557800636-894a64c1696f?auto=format&fit=crop&q=80&w=800',
-    stripePriceId: 'price_YOUR_HASSAKU_ID' 
+    stripePriceId: 'price_1SyBZh1GyBXkmkVBCsnXB1LF' 
   },
   { 
     id: 3, 
@@ -91,7 +91,7 @@ const PRODUCTS = [
     acid: 1.1, 
     size: 'L', 
     img: 'https://images.unsplash.com/photo-1591206111081-1756b342e811?auto=format&fit=crop&q=80&w=800',
-    stripePriceId: 'price_YOUR_SHIRANUI_ID'
+    stripePriceId: 'price_1SyBa51GyBXkmkVBmIWs60Pj'
   },
 ]
 
